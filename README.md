d3e4786742cd5dca8df09cd6e59fe8e9cfcae09e
Clivern <hello@clivern.com>
2020-11-25 23:14:33 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Pollux
-🐺 Run Kubernetes on AWS with Terraform.
+### Usage
+
+1. Create `terraform.tfvars` and define required vars.
+
+```zsh
+access_key        = "~~"
+secret_key        = "~~"
+```
+
+or add as environment variables.
+
+```zsh
+export access_key=...
+export secret_key=...
+```
+
+2. Create a digitalocean token from digitalocean dashboard and then run `terraform init`.
+
+3. Run `terraform plan` to check the changes.
+
+4. Run `terraform apply` to perform the action.
---
+++ provider.tf
@@ -0,0 +1,35 @@
+# Copyright 2022 Spacewalk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15"
+    }
+  }
+
+  required_version = ">= 1.0.7"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "pollux-tf-state"
+    key            = "pollux/terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "pollux-tf-locks"
+    encrypt        = true
+    profile        = "default"
+  }
+}
